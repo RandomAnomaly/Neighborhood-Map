28a86bd455365e468404a754a0e4f04137398631
jack <jack.gammon@equinox.co.nz>
2017-01-11 15:44:52 +1300

    Code now shows google map

---
+++ dev/scripts/model/model.js
@@ -0,0 +1,5 @@
+var initialMarkers = [{
+    "name": "Uluru",
+    "position": { "lat": -25.345, "lon": 131.036111 }
+}
+];
---
+++ dev/scripts/viewmodel/viewmodel.js
@@ -0,0 +1,20 @@
+var ViewModel = function(){
+    
+
+    initMap = function(){
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+    }();
+}
+
+
+var applyBindings = function(){
+    ko.applyBindings(new ViewModel());
+}
