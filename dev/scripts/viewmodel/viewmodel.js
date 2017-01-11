187ecdaee8fd4b6ba64be452299d00d8e8c61d81
jack <jack.gammon@equinox.co.nz>
2017-01-11 16:04:22 +1300

    Map now uses knockout

--- dev/scripts/viewmodel/viewmodel.js
+++ dev/scripts/viewmodel/viewmodel.js
@@ -1,20 +1,65 @@
-var ViewModel = function(){
-    
+var ViewModel = function () {
+    var self = this;
 
-    initMap = function(){
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-    }();
+    // var uluru = { lat: -25.363, lng: 131.044 };
+
+    // var el = document.createElement('div');
+
+    // var map = new google.maps.Map(el, {
+    //     zoom: 4,
+    //     center: uluru
+    // });
+
+    // this.myMap = ko.observable(el);
+    // // initMap = function(){
+
+
+    // //     var marker = new google.maps.Marker({
+    // //       position: uluru,
+    // //       map: map
+    // //     });
+    // // }();
+
+    self.myMap = ko.observable({
+        lat: ko.observable(55),
+        lng: ko.observable(11)
+    });
+
+    
 }
 
 
-var applyBindings = function(){
+
+
+
+// CUSTOM BINDINGS
+
+// Custom binding from http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs
+ko.bindingHandlers.map = {
+
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var mapObj = ko.utils.unwrapObservable(valueAccessor());
+        var latLng = new google.maps.LatLng(
+            ko.utils.unwrapObservable(mapObj.lat),
+            ko.utils.unwrapObservable(mapObj.lng));
+        var mapOptions = {
+            center: latLng,
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        mapObj.googleMap = new google.maps.Map(element, mapOptions);
+    }
+
+
+};
+
+
+
+
+
+
+var applyBindings = function () {
     ko.applyBindings(new ViewModel());
 }
+
