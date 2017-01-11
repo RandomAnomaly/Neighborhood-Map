675e67a01b94c3049df10cdda34e1e1175a982a8
jack <jack.gammon@equinox.co.nz>
2017-01-11 16:20:42 +1300

    Map now defaults to wellington

--- dev/scripts/viewmodel/viewmodel.js
+++ dev/scripts/viewmodel/viewmodel.js
@@ -1,31 +1,38 @@
+var ListView = function () {
+
+}
+
 var ViewModel = function () {
-    var self = this;
+	var self = this;
 
-    // var uluru = { lat: -25.363, lng: 131.044 };
+	// var uluru = { lat: -25.363, lng: 131.044 };
 
-    // var el = document.createElement('div');
+	// var el = document.createElement('div');
 
-    // var map = new google.maps.Map(el, {
-    //     zoom: 4,
-    //     center: uluru
-    // });
+	// var map = new google.maps.Map(el, {
+	//     zoom: 4,
+	//     center: uluru
+	// });
 
-    // this.myMap = ko.observable(el);
-    // // initMap = function(){
+	// this.myMap = ko.observable(el);
+	// // initMap = function(){
 
 
-    // //     var marker = new google.maps.Marker({
-    // //       position: uluru,
-    // //       map: map
-    // //     });
-    // // }();
+	// //     var marker = new google.maps.Marker({
+	// //       position: uluru,
+	// //       map: map
+	// //     });
+	// // }();
 
-    self.myMap = ko.observable({
-        lat: ko.observable(55),
-        lng: ko.observable(11)
-    });
+//-41.288889,174.777222
 
-    
+	self.myMap = ko.observable({
+		lat: ko.observable(-41.288889),
+		lng: ko.observable(174.777222)
+	});
+	
+	this.cl = function () {
+	}
 }
 
 
@@ -37,19 +44,19 @@ var ViewModel = function () {
 // Custom binding from http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs
 ko.bindingHandlers.map = {
 
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var mapObj = ko.utils.unwrapObservable(valueAccessor());
-        var latLng = new google.maps.LatLng(
-            ko.utils.unwrapObservable(mapObj.lat),
-            ko.utils.unwrapObservable(mapObj.lng));
-        var mapOptions = {
-            center: latLng,
-            zoom: 5,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+	init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+		var mapObj = ko.utils.unwrapObservable(valueAccessor());
+		var latLng = new google.maps.LatLng(
+			ko.utils.unwrapObservable(mapObj.lat),
+			ko.utils.unwrapObservable(mapObj.lng));
+		var mapOptions = {
+			center: latLng,
+			zoom: 18,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
 
-        mapObj.googleMap = new google.maps.Map(element, mapOptions);
-    }
+		mapObj.googleMap = new google.maps.Map(element, mapOptions);
+	}
 
 
 };
@@ -60,6 +67,6 @@ ko.bindingHandlers.map = {
 
 
 var applyBindings = function () {
-    ko.applyBindings(new ViewModel());
+	ko.applyBindings(new ViewModel());
 }
 
