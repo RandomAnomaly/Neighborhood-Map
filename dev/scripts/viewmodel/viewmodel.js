5ebf2ba49cee74c96e51f1e1d154c1d692330b57
jack <jack.gammon@equinox.co.nz>
2017-01-12 13:48:44 +1300

    Moved the binding into the viewmodel

--- dev/scripts/viewmodel/viewmodel.js
+++ dev/scripts/viewmodel/viewmodel.js
@@ -6,48 +6,49 @@ var ViewModel = function () {
 
   self.myMap = ko.observable({});
 
+  // CUSTOM BINDINGS
+
+  // Based on custom binding from http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs
+  ko.bindingHandlers.map = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+      var mapObj = ko.utils.unwrapObservable(valueAccessor());
+
+      var mapOptions = {
+        center: model.position,
+        zoom: model.mapDefaultZoom,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: model.mapStyles
+      };
+
+      mapObj.googleMap = new google.maps.Map(element, mapOptions);
+      service = new google.maps.places.PlacesService(mapObj.googleMap);
+
+      var request = {
+        location: model.position,
+        radius: model.searchRadius,
+        types: model.placesSearchTypes
+      };
+
+      service.nearbySearch(request, function (results, status) {
+        // TODO: error handling
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i += 1) {
+            new google.maps.Marker({
+              map: mapObj.googleMap,
+              position: results[i].geometry.location,
+              title: results[i].name
+            })
+          }
+          console.log(results[0]);
+        }
+      });
+    }
+  };
   this.cl = function () {
   }
 }
 
-// CUSTOM BINDINGS
 
-// Based on custom binding from http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs
-ko.bindingHandlers.map = {
-  init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-    var mapObj = ko.utils.unwrapObservable(valueAccessor());
-    
-    var mapOptions = {
-      center: model.position,
-      zoom: model.mapDefaultZoom,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      styles: model.mapStyles
-    };
-
-    mapObj.googleMap = new google.maps.Map(element, mapOptions);
-    service = new google.maps.places.PlacesService(mapObj.googleMap);
-
-    var request = {
-      location: model.position,
-      radius: model.searchRadius,
-      types: model.placesSearchTypes
-    };
-
-    service.nearbySearch(request, function (results, status) {
-      // TODO: error handling
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i += 1) {
-          new google.maps.Marker({
-            map: mapObj.googleMap,
-            position: results[i].geometry.location,
-            title: results[i].name
-          })
-        }
-        console.log(results[0]);
-      }
-    });
-  }
-};
 
 var applyBindings = function () {
   ko.applyBindings(new ViewModel());
