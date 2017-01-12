a19760088e7f44ce9b1147665b3dd63c90bafc0b
jack <jack.gammon@equinox.co.nz>
2017-01-12 13:20:19 +1300

    Moved map styles into model

--- dev/scripts/model/model.js
+++ dev/scripts/model/model.js
@@ -1,17 +1,73 @@
-var initialMarkers = [
-	{
-		"name": "Uluru",
-		"position": { "lat": -25.345, "lon": 131.036111 }
-	}
-];
-
-
 //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDTn_z5rWDu3IH_e2dNRG0F2-J0oO9LTa4
 
 var model = {
-	data: [],
-	
-	init: function (map) {
-
-	}
+	mapStyles: [
+		{ elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+		{ elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+		{ elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+		{
+			featureType: 'administrative.locality',
+			elementType: 'labels.text.fill',
+			stylers: [{ color: '#d59563' }]
+		},
+		{
+			featureType: 'poi',
+			stylers: [{ visibility: 'off' }]
+		},
+		{
+			featureType: 'road',
+			elementType: 'geometry',
+			stylers: [{ color: '#38414e' }]
+		},
+		{
+			featureType: 'road',
+			elementType: 'geometry.stroke',
+			stylers: [{ color: '#212a37' }]
+		},
+		{
+			featureType: 'road',
+			elementType: 'labels.text.fill',
+			stylers: [{ color: '#9ca5b3' }]
+		},
+		{
+			featureType: 'road.highway',
+			elementType: 'geometry',
+			stylers: [{ color: '#746855' }]
+		},
+		{
+			featureType: 'road.highway',
+			elementType: 'geometry.stroke',
+			stylers: [{ color: '#1f2835' }]
+		},
+		{
+			featureType: 'road.highway',
+			elementType: 'labels.text.fill',
+			stylers: [{ color: '#f3d19c' }]
+		},
+		{
+			featureType: 'transit',
+			elementType: 'geometry',
+			stylers: [{ color: '#2f3948' }]
+		},
+		{
+			featureType: 'transit.station',
+			elementType: 'labels.text.fill',
+			stylers: [{ color: '#d59563' }]
+		},
+		{
+			featureType: 'water',
+			elementType: 'geometry',
+			stylers: [{ color: '#17263c' }]
+		},
+		{
+			featureType: 'water',
+			elementType: 'labels.text.fill',
+			stylers: [{ color: '#515c6d' }]
+		},
+		{
+			featureType: 'water',
+			elementType: 'labels.text.stroke',
+			stylers: [{ color: '#17263c' }]
+		}
+	]
 }
